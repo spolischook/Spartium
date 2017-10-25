541d68c389a4f4e5af945697f31e81c6293a7a54
Serhii Polishchuk <spolischook@gmail.com>
2017-10-25 20:20:11 +0300

    Added push to splited repository

--- split.sh
+++ split.sh
@@ -3,6 +3,7 @@
 TEMP_PROJECT_DIR=/tmp/project
 BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
 FOLDER_NAME=src/Spolischook/Spartium
+REPOSITORY_URL=git@github.com:spolischook/Spartium.git
 
 rm -rf $TEMP_PROJECT_DIR
 cp -r ./ $TEMP_PROJECT_DIR
@@ -13,3 +14,5 @@ find $TEMP_PROJECT_DIR -type d -name vendor -exec rm -rf {} \;
 
 git filter-branch --prune-empty --subdirectory-filter $FOLDER_NAME  $BRANCH_NAME
 
+git remote set-url origin $REPOSITORY_URL
+git push -u origin $BRANCH_NAME
